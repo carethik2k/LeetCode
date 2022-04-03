23ec1230e4457b5e0901b37806ce7aa8bb50de55
Manju Muthukumaran <meetmanjusri@gmail.com>
2022-04-03 15:54:43 -0500

    Updates

--- TwoSum.cs
+++ TwoSum.cs
@@ -1,14 +1,26 @@
 public class TwoSumSolution{
     public int[] TwoSum(int[] nums, int target) 
     {
+        // for(int i=0; i<= nums.Length-1;i++)
+        // {
+        //     for(int j= i+1; j<=nums.Length-1; j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //             return new int[]{i, j};
+        //     }
+        // }
+
+        Dictionary<int,int> dictNum = new Dictionary<int, int>();
         for(int i=0; i<= nums.Length-1;i++)
         {
-            for(int j= i+1; j<=nums.Length-1; j++)
+            var targetNum = target - nums[i];
+            if(dictNum.ContainsKey(targetNum))
             {
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i, j};
+                return new int[]{dictNum[targetNum],i};
             }
-        }    
+            dictNum.TryAdd(nums[i],i);
+        }
+
         return new int[1]{0};
     }
 }
